4977e6bbdf52f0c3dc6d448a3c71e75bd151a667
Teresa E. Gimeno <tegimeno@gmail.com>
2021-10-13 16:21:31 +0200

    Script for unzipping files automatically

--- unzipFilesMODIS.R
+++ unzipFilesMODIS.R
@@ -3,12 +3,9 @@ library(tidyverse)
 # enter your the name of the fodler where you have the data accordingly
 zipFileNames <- paste0("211013/" , list.files('211013/'))
 
-# create a vector with only the names of the *.csv (what we want)
-csvFileNamesL <- map(zipFileNames, ~ unzip(zipfile = .x, list = T)[1, 1])
-csvFileNames <- c()
-for (i in 1:length(csvFileNamesL)){
-  csvFileNames[i] <- csvFileNamesL[[i]][1]
-}
+# create a vector (from a list) with only the names of the *.csv (what we want)
+csvFileNames <- unlist(map(zipFileNames, ~ unzip(zipfile = .x, list = T)[1, 1]))
 
 # extract the files onto a newly created folder within the data folder
 purrr::map2(zipFileNames, csvFileNames, ~unzip(zipfile = .x, file = .y, exdir = '211013/AquaUnzipped'))
+# map2 allows to pass two arguments
