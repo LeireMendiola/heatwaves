f78209fe38ecd8ec8ea65d16762265d4a7bc2ed7
LeireMendiola <leiremendiola@gmail.com>
2022-02-22 11:09:55 +0100

    CalcH script update

--- Scripts/calcH_deltaT.R
+++ Scripts/calcH_deltaT.R
@@ -13,11 +13,11 @@ full_data <- full_data %>%
 
 data_21 <- filter(full_data, Date >= as.Date("2021-05-01"), Date <= as.Date("2021-09-30"))
 
-#we change format 
+#we select only the variables we need
 
-select("ID", "Date", "LST_modis", "dbh", "density", "lai", "topo_altitude_asl","HM", "Max" )
+data_21 <- data_21 %>%
+  select("ID", "Date", "LST_modis", "dbh", "density", "lai", "topo_altitude_asl","HM", "MaxTemperature", "MinRelativeHumidity", "WindSpeed" )
 
-data_21 <- data_21 %>% select (-"MeanTemperature.x", -"MinTemperature.x", -"MaxTemperature.x", "Precipitation.x",-"MeanRelativeHumidity.x", -"MinRelativeHumidity.x", -"MaxRelativeHumidity.x", -"Radiation.x", -"WindSpeed.x", -"WindDirection.x", -"PET.x", -"Precipitation.x", -"X.1", -"X") #cleaning dataset
 
 ### calculate sensible heat from DeltaT step by step ###
 
@@ -27,44 +27,45 @@ data_21 <- data_21 %>% mutate(VPDmax = plantecophys::RHtoVPD(RH=MinRelativeHumid
 ##mirar por que tengo tantos NAs
  
 # calculate air pressure from altitude
+
 data_21 <- data_21 %>% 
   mutate(airP = bigleaf::pressure.from.elevation(elev = topo_altitude_asl,
                                                  Tair = MaxTemperature, VPD = VPDmax, constants = bigleaf::bigleaf.constants()))
 
 # calculate air density for a given air temperature (Tair) and pressure
+
 data_21 <- data_21 %>% 
   mutate(rho = bigleaf::air.density(Tair = MaxTemperature, pressure = airP, constants = bigleaf::bigleaf.constants()))
 
 # calculate zero plane displacement, d, from vegetation height as d = 2/3 VH, where VH is mean vegetation height (Brutsaert 1982)
+
 data_21 <- data_21 %>%  mutate(d = (2/3)*HM)
 
 # calculate the momentum roughness parameter in m, z0m, as z0m = 0.1 VH (Brutsaert 1982)
+
 data_21 <- data_21 %>% mutate(z0m = 0.1*HM)
 
 # calculate friction velocity (u*, here uS) from vegetation height and wind speed
 
 #but first Adrià's correction for wind velocity
-data_21 <- data_21 %>% mutate(windS = 1.635*WindSpeed - 0.1405*(WindSpeed)^2 + 0.00637*(WindSpeed)^3 + 0.3416)
 
-#same but with a function
-ws_correction <- function(windS){windS <- 0.3416 + (1.635*WindS) - (0.1405*WindSy^2) + (0.00637*WindS^3)} 
+data_21 <- data_21 %>% mutate(windS_correct = 1.635*WindSpeed - 0.1405*(WindSpeed)^2 + 0.00637*(WindSpeed)^3 + 0.3416)
 
-data_21 <- data_21 %>% mutate(WindS_correct = ws_correction(WindS = WindSpeed.y)) #i dont know why it doesnt work
-
-data_21 <- data_21 %>% rename(WindS_correct=windS)
 
 # Eqn 5.1 in Campbell & Norman 1998
+
 data_21 <- data_21 %>%
-  mutate(uS = 0.4 * WindS_correct/log((HM + 2 - d)/z0m))
+  mutate(uS = 0.4 * windS_correct/log((HM + 2 - d)/z0m))
 
 # calculate resistance to heat transfer (rH) from Eqn 5 in Knauer et al. 2017 GCB (rH = 1/Ga) CORRIGENDUM
 
 # rH calculated from wind speed u (m/s) and friction velocity uS (m/s) under the assumption of a logarithmic wind profile above the canopy
+
 calcrH <- function(u, uS){
   rH <- (u/uS^2) + 6.2 * uS^-0.67
   return(rH)
 }
-data_21 <- data_21 %>% mutate(rH = calcrH(u = WindS_correct, uS = uS))
+data_21 <- data_21 %>% mutate(rH = calcrH(u = windS_correct, uS = uS))
 
 # heat capacity of air in J kg-1 K-1
 cp <- 1003.5
@@ -83,6 +84,12 @@ data_21 <- data_21 %>% mutate (DeltaT = LST_modis - (MaxTemperature+273))
 
 data_21 <- data_21 %>% 
   mutate(H = calcH(cp = cp, rho = rho, dT = DeltaT, rH = rH))
+
+#explore H values 
+hist(data_21$H_neg)
+
+H_neg <- data_21[data_21$H < 0, ] #almost 50% of observations are negative values
+
 
 # get rid of unreasonable values
 data_21[which(data_21$H > 1500), 'H'] <- NA  #0.92% of observations are NA
